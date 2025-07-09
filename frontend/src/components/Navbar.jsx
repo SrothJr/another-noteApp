7b4c61744b3b7ef7aa2d686983704c56405d698b
SrothJr <nazimhossain14602@gmail.com>
2025-07-09 18:46:01 +0600

    navBar update

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Link } from "react-router";
 import { PlusIcon } from "lucide-react";
 const Navbar = () => {
@@ -7,7 +6,7 @@ const Navbar = () => {
       <div className="mx-auto max-w-6xl p-4">
         <div className="flex items-center justify-between">
           <h1 className="text-3xl font-bold text-primary font-mono tracking-tight">
-            ThinkBoard
+            anotherNoteApp
           </h1>
           <div className="flex items-center gap-4">
             <Link to={"/create"} className="btn btn-primary">
